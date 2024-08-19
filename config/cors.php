9107119cfe205a16e2e2a83aad11e5271e21afd9
nam.dovan1 <nam.dovan1@vti.com.vn>
2024-08-19 21:25:46 +0700

    add cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:3002'], // Chỉ định các domain được phép
+    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'http://localhost:3002')], // Chỉ định các domain được phép
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
