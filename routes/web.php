eb7f2e2f5761ead50244b5d192d1cad5bce30ef0
dnamxtvl <kakitani2000@gmail.com>
2025-08-31 04:19:46 +0700

    fix: optimize dockerfile

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,8 @@ use Illuminate\Support\Facades\Route;
 use Stevebauman\Location\Facades\Location;
 
 Route::get('/test/', function () {
-    //dd(Location::get('116.96.46.236'));
     Log::error('test error channel');
     Log::info('test info channel');
     Log::debug('test debug channel');
-    Log::warning('test warning channel');
     dd(\App\Models\Category::all()->pluck('name')->toArray());
 });
