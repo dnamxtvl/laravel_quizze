0260aff0a5a1f2c5fdab62c84dd1bb920ccbe479
dnamxtvl <kakitani2000@gmail.com>
2025-08-31 14:41:13 +0700

    fix: optimize dockerfile

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,5 @@ Route::get('/test/', function () {
     Log::error('test error channel');
     Log::info('test info channel');
     Log::debug('test debug channel');
-    Log::warning('test warning channel');
     dd(\App\Models\Category::all()->pluck('name')->toArray());
 });
