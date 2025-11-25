afe93569ed5545fc8b040d681146568570f58e66
nam.dovan1 <nam.dovan1@vti.com.vn>
2025-11-25 16:25:20 +0700

    fix: add healthcheck

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Stevebauman\Location\Facades\Location;
 
+Route::get('/healthcheck', function () {
+    return response()->json(['status' => 'OK'], 200);
+});
+
 Route::get('/test/', function () {
     Log::error('test error channel');
     dd(\App\Models\Category::all()->pluck('name')->toArray());
