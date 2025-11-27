a4ad4724cbd06af00dbb93255df5f0df2f31e6b0
dnamxtvl <kakitani2000@gmail.com>
2025-11-28 00:29:07 +0700

    fix: test mail ses

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,23 @@
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Stevebauman\Location\Facades\Location;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/healthcheck', function () {
     return response()->json(['status' => 'OK'], 200);
 });
 
 Route::get('/test/', function () {
+    Log::error('test error channel');
+    dd(\App\Models\Category::all()->pluck('name')->toArray());
+});
+
+Route::get('/test-mail/', function () {
+    $email = 'hoangthanh22082000@gmail.com';
+    Mail::raw('This is a test email from Laravel SES', function ($message) use ($email) {
+        $message->to($email)
+            ->subject('Test Email from Laravel');
+    });
     Log::error('test error channel');
     dd(\App\Models\Category::all()->pluck('name')->toArray());
 });
